d60f3e32028f8af791daaff52ad5ddc63e0b782d
Pratanu Mandal <pmandal5@en4174514-scai.3500.dhcp.asu.edu>
2024-02-01 15:21:49 -0700

    Updated metric.py

--- demo/modules/metric.py
+++ demo/modules/metric.py
@@ -1,16 +1,10 @@
-import logging
-
-import pandas as pd
-from bunch_py3 import bunchify
-
-from commons.utils import execute_and_report
 from modules.module import Module
 
 
-class Dataset(Module):
+class Metric(Module):
 
     def __init__(self, module_id: int = None):
-        super().__init__(module_id, 'schema/metric.json')
+        super().__init__(module_id, 'metric')
 
     def instantiate(self):
         # TODO: Create the structure of the new instance
@@ -21,6 +15,6 @@ class Dataset(Module):
         if module_id == 0:
             return 'metric/shd'
 
-    def execute(self, ground, pred):
+    def evaluate(self, ground, pred):
         # TODO: Execute the metric, change input parameters if necessary
         pass
