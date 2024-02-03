b72edd2e2c7566e1ffd7e773a0cdcb214a8327a4
Shu Wan <shu.wan@outlook.com>
2024-02-02 17:49:02 -0700

    Revert "fix conflicts"
    This reverts commit cff1f6f98212790d62fe5619871e5b3a02ef840b.

--- demo/modules/metric.py
+++ demo/modules/metric.py
@@ -6,6 +6,11 @@ from bunch_py3 import Bunch, bunchify
 from commons.utils import parse_arguments, execute_and_report
 from modules.module import Module
 
+<<<<<<< HEAD
+import os
+=======
+
+>>>>>>> f84d8496ef636391ef11b320a91d33514c2ec78c
 class Metric(Module):
 
     def __init__(self, module_id: int = None):
@@ -35,11 +40,18 @@ class Metric(Module):
         metric_args = {}
 
         if self.task == 'discovery':
+<<<<<<< HEAD
+            ground_truth = arguments.ground_truth
+            prediction = arguments.prediction
+            mpath = os.path.join(self.base_dir, self.path)
+            result = execute_and_report(mpath, self.name, pred=prediction, truth=ground_truth)
+            return result
+=======
             metric_args[self.inputs.ground.id] = arguments.ground_truth
             metric_args[self.inputs.prediction.id] = arguments.prediction
 
         # execute the metric
-        result = execute_and_report(file_path, self.name, **metric_args)
+        result = execute_and_report(file_path, "evaluate", **metric_args)
 
         # map the outputs
         output = {}
@@ -49,3 +61,4 @@ class Metric(Module):
         logging.info('Executed metric successfully')
 
         return bunchify(output)
+>>>>>>> f84d8496ef636391ef11b320a91d33514c2ec78c
