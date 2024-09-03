a8a0fa7d1b10ecffe8d8874c149fb46631c6ca4b
ertugrulcobann <ertugrul06cb@gmail.com>
2024-09-03 12:36:37 -0700

    Fix conda install

--- causalbench-asu/tests/test.sh
+++ causalbench-asu/tests/test.sh
@@ -1,7 +1,3 @@
-mkdir -p ~/miniconda3
-curl https://repo.anaconda.com/miniconda/Miniconda3-py39_24.5.0-0-MacOSX-x86_64.sh -o ~/miniconda3/miniconda.sh
-bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
-rm ~/miniconda3/miniconda.sh
 conda create -n causal-test python=3.10 -y
 conda activate causal-test
 pip install torch
