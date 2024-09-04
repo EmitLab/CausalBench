d813b9edf8915148a7ff0b7c1e4f6ee0617d8391
ertugrulcobann <134216135+ertugrulcobann@users.noreply.github.com>
2024-09-03 20:13:38 -0700

    Update test.sh

--- causalbench-asu/tests/test.sh
+++ causalbench-asu/tests/test.sh
@@ -1,10 +1,10 @@
 conda create -n causal-test python=3.10 -y
 conda activate causal-test
-python3 -m pip install --upgrade pip
+python -m pip install --upgrade pip
 cd ../..    
-pip3 install torch
-pip3 install build
+pip install torch
+pip install build
 python -m build
-pip3 install dist/causalbench_asu-0.1rc1-py3-none-any.whl 
+pip install dist/causalbench_asu-0.1rc1-py3-none-any.whl 
 cd causalbench-asu/tests
-python3 execute.py 
+python execute.py 
