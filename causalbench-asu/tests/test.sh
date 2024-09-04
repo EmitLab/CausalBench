57ab303400b8fec6031a03367e7856ff54dc91a8
ertugrulcobann <134216135+ertugrulcobann@users.noreply.github.com>
2024-09-03 20:49:39 -0700

    Update test.sh

--- causalbench-asu/tests/test.sh
+++ causalbench-asu/tests/test.sh
@@ -1,8 +1,8 @@
-conda create -n causal-test python=3.10.14 -y
+conda create -n causal-test python=3.10.6 -y
 conda activate causal-test
 python -m pip install --upgrade pip
 cd ../..    
-pip install torch==2.3.0 --index-url https://download.pytorch.org/whl/cpu-cxx11-abi/torch-2.3.0%2Bcpu.cxx11.abi-cp310-cp310-linux_x86_64.whl
+pip install torch==2.3.0 --index-url https://download.pytorch.org/whl/torch_stable.html
 pip install build
 python -m build
 pip install dist/causalbench_asu-0.1rc1-py3-none-any.whl 
