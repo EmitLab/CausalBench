29d403cb4c698d3ce39754ed81ce9205c18231f3
ertugrulcobann <134216135+ertugrulcobann@users.noreply.github.com>
2024-09-05 13:41:35 -0700

    Update test.sh

--- causalbench-asu/tests/test.sh
+++ causalbench-asu/tests/test.sh
@@ -4,6 +4,7 @@ conda activate causal-test
 cd ../..    
 echo "Building package..."
 pip install gcastle
+pip install torch
 pip install build
 python -m build
 pip install dist/causalbench_asu-0.1rc3-py3-none-any.whl 
