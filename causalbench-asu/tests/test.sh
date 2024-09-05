c2c5bd673a889bdcba5c5d5fffeb32c02ef3894a
Ahmet Kapkic <akapkic@asu.edu>
2024-09-05 11:01:49 -0700

    Update test.sh
    Added print statements

--- causalbench-asu/tests/test.sh
+++ causalbench-asu/tests/test.sh
@@ -1,9 +1,12 @@
+echo "Creating conda environment..."
 conda create -n causal-test python=3.10 -y
 conda activate causal-test
 cd ../..    
-python -m pip install torch==2.3
+echo "Building package..."
+#python -m pip install torch==2.3
 pip install build
 python -m build
 pip install dist/causalbench_asu-0.1rc1-py3-none-any.whl 
 cd causalbench-asu/tests
+echo "Tests executing"
 python execute.py 
