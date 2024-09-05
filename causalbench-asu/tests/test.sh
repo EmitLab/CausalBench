3b6f41f0fbf36c801565877e035c2166e565eacd
ertugrulcobann <134216135+ertugrulcobann@users.noreply.github.com>
2024-09-05 12:53:06 -0700

    Update test.sh

--- causalbench-asu/tests/test.sh
+++ causalbench-asu/tests/test.sh
@@ -3,7 +3,7 @@ conda create -n causal-test python=3.10 -y
 conda activate causal-test
 cd ../..    
 echo "Building package..."
-#python -m pip install torch==2.3
+pip install castle
 pip install build
 python -m build
 pip install dist/causalbench_asu-0.1rc3-py3-none-any.whl 
