5cce937db8acd8fbce0394634290f15567828bc6
Pratanu Mandal <pratanu.mandal@gmail.com>
2024-09-26 14:22:50 -0700

    Partial implementation of disk IO profiling on macOS

--- causalbench-asu/causalbench/commons/disk.py
+++ causalbench-asu/causalbench/commons/disk.py
@@ -48,15 +48,20 @@ class Disks:
                     physical_drives[drive_id].usage.total += int(partition.Size)
                     physical_drives[drive_id].usage.free += int(partition.FreeSpace)
 
-            physical_drives[drive_id].usage.used = physical_drives[drive_id].usage.total - physical_drives[drive_id].usage.free
+            physical_drives[drive_id].usage.used = physical_drives[drive_id].usage.total - physical_drives[
+                drive_id].usage.free
 
-            result = subprocess.run(['powershell', '-Command', f'Get-PhysicalDisk | Select-Object DeviceID, MediaType | Where-Object {{ $_.DeviceID -eq {drive.Index} }}'], capture_output=True, text=True)
+            result = subprocess.run(['powershell', '-Command',
+                                     f'Get-PhysicalDisk | Select-Object DeviceID, MediaType | Where-Object {{ $_.DeviceID -eq {drive.Index} }}'],
+                                    capture_output=True, text=True)
             if 'SSD' in result.stdout:
                 physical_drives[drive_id].mediatype = 'SSD'
             elif 'HDD' in result.stdout:
                 physical_drives[drive_id].mediatype = 'HDD'
             else:
                 physical_drives[drive_id].mediatype = 'Unknown'
+
+            physical_drives[drive_id].fusion = False
 
         return physical_drives
 
@@ -90,6 +95,8 @@ class Disks:
             else:
                 physical_drives[drive_id].mediatype = 'Unknown'
 
+            physical_drives[drive_id].fusion = False
+
         return physical_drives
 
     @staticmethod
@@ -98,26 +105,27 @@ class Disks:
         plist = plistlib.loads(result.stdout.encode())
 
         disks = dict()
-
+        fusion_list = dict()
         for disk in plist['AllDisks']:
             disk_info = subprocess.run(['diskutil', 'info', '-plist', disk], stdout=subprocess.PIPE, text=True)
             disk_plist = plistlib.loads(disk_info.stdout.encode())
 
-            if disk_plist.get('MountPoint'):
-                parent = disk_plist.get('ParentWholeDisk')
-                if parent not in disks:
-                    disks[parent] = []
-                disks[parent].append(disk)
+            if disk_plist.get('MountPoint') and disk_plist.get('APFSPhysicalStores'):
 
+                for parent in disk_plist.get('APFSPhysicalStores'):
+                    parent = parent["APFSPhysicalStore"]
+                    if disk_plist.get('Fusion') and parent not in fusion_list:
+                        fusion_list[parent] = disk_plist.get('APFSContainerReference')
+                    if parent not in disks:
+                        disks[parent] = []
+                    disks[parent].append(disk)
         physical_drives = Bunch()
 
         for disk, partitions in disks.items():
             disk_info = subprocess.run(['diskutil', 'info', '-plist', disk], stdout=subprocess.PIPE, text=True)
             disk_plist = plistlib.loads(disk_info.stdout.encode())
-
             physical_drives[disk] = Bunch()
-
-            physical_drives[disk].model = disk_plist.get('MediaName')
+            physical_drives[disk].parent = disk_plist.get('ParentWholeDisk')
 
             physical_drives[disk].usage = Bunch()
             physical_drives[disk].usage.total = 0
@@ -126,11 +134,8 @@ class Disks:
             for partition in partitions:
                 part_info = subprocess.run(['diskutil', 'info', '-plist', partition], stdout=subprocess.PIPE, text=True)
                 part_plist = plistlib.loads(part_info.stdout.encode())
-
-                physical_drives[disk].usage.total += part_plist.get('TotalSize')
+                physical_drives[disk].usage.total = part_plist.get('TotalSize')
                 physical_drives[disk].usage.used += part_plist.get('CapacityInUse')
-
-            physical_drives[disk].usage.free = physical_drives[disk].usage.total - physical_drives[disk].usage.used
 
             if 'SolidState' in disk_plist:
                 if disk_plist.get('SolidState'):
@@ -139,8 +144,28 @@ class Disks:
                     physical_drives[disk].mediatype = 'HDD'
             else:
                 physical_drives[disk].mediatype = 'Unknown'
+        result = dict()
+        for name, disk in physical_drives.items():
+            if disk.parent not in result.keys():
+                result[disk.parent] = Bunch()
+                result[disk.parent].mediatype = disk.mediatype
+                result[disk.parent].total = 0
+                result[disk.parent].used = 0
+                if name in fusion_list:
+                    result[disk.parent].fusion = fusion_list[name]
+                else:
+                    result[disk.parent].fusion = None
 
-        return physical_drives
+            result[disk.parent].total = disk.usage.total
+            result[disk.parent].used += disk.usage.used
+
+        for disk in result:
+            result[disk].free = result[disk].total - result[disk].used
+            drive_info = subprocess.run(['diskutil', 'info', '-plist', disk], stdout=subprocess.PIPE, text=True)
+            drive_plist = plistlib.loads(drive_info.stdout.encode())
+            result[disk].mediaName = drive_plist.get('MediaName')
+
+        return result
 
 
 class DisksProfiler:
@@ -151,8 +176,8 @@ class DisksProfiler:
 
     def _get_usage(self) -> Bunch:
         usage: dict = psutil.disk_io_counters(perdisk=True)
-        print(usage)
-        usage: dict = {key: {'read_bytes': value.read_bytes, 'write_bytes': value.write_bytes} for key, value in usage.items() if key in self.disks}
+        usage: dict = {key: {'read_bytes': value.read_bytes, 'write_bytes': value.write_bytes} for key, value in
+                       usage.items() if key in self.disks}
         usage: dict = dict(sorted(usage.items()))
         return bunchify(usage)
 
@@ -162,8 +187,8 @@ class DisksProfiler:
         usage: dict = dict()
         for key in self._usage.keys():
             usage[key] = dict()
-            usage[key]['read_bytes'] = current_usage[key]['read_bytes'] - current_usage[key]['read_bytes']
-            usage[key]['write_bytes'] = current_usage[key]['write_bytes'] - current_usage[key]['write_bytes']
+            usage[key]['read_bytes'] = current_usage[key]['read_bytes'] - self._usage[key]['read_bytes']
+            usage[key]['write_bytes'] = current_usage[key]['write_bytes'] - self._usage[key]['write_bytes']
         return bunchify(usage)
 
 
@@ -179,8 +204,6 @@ if __name__ == "__main__":
     disk: Disks = get_disks()
     for k, v in disk.disk_drives.items():
         print(k, v)
-
-    print()
 
     profiler: DisksProfiler = DisksProfiler(disk)
     for k, v in profiler.usage.items():
