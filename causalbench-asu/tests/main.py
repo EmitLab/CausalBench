df1ba5ef866406a22c3d842a882925e1eea65209
Pratanu Mandal <pratanu.mandal@gmail.com>
2026-02-12 16:22:59 -0700

    Tested and fixed modules for ML tasks

--- causalbench-asu/tests/main.py
+++ causalbench-asu/tests/main.py
@@ -4,110 +4,115 @@ from causalbench.modules import Dataset, Metric, Model, Context, Run, Task
 def main():
     pass
 
-    task = Task(zip_file="C:/Users/prata/Files/Git/CausalBench/causalbench-asu/tests/task/regression.zip").load()
+    # task = Task(zip_file="C:/Users/prata/Files/Git/CausalBench/causalbench-asu/tests/task/classification.zip").load()
+    # task = Task(zip_file="C:/Users/prata/Files/Git/CausalBench/causalbench-asu/tests/task/regression.zip").load()
 
-    # dataset = Dataset(zip_file="C:/Users/prata/Files/Git/CausalBench/causalbench-asu/tests/data/california_housing.zip")
-    dataset = Dataset(zip_file="C:/Users/prata/Files/Git/CausalBench/causalbench-asu/tests/data/breast_cancer_wisconsin.zip")
-    data_files = dataset.load()
+    # # dataset = Dataset(zip_file="C:/Users/prata/Files/Git/CausalBench/causalbench-asu/tests/data/breast_cancer_wisconsin.zip")
+    # # dataset = Dataset(zip_file="C:/Users/prata/Files/Git/CausalBench/causalbench-asu/tests/data/forest_covertype.zip")
+
+    # # dataset = Dataset(zip_file="C:/Users/prata/Files/Git/CausalBench/causalbench-asu/tests/data/california_housing.zip")
+    # dataset = Dataset(zip_file="C:/Users/prata/Files/Git/CausalBench/causalbench-asu/tests/data/diabetes.zip")
+
+    # data_files = dataset.load()
+
+    # # model = Model(zip_file="C:/Users/prata/Files/Git/CausalBench/causalbench-asu/tests/model/logistic_regression.zip")
+    # # out = model.execute(Bunch({'data': data_files.file1.copy(deep=True), 'penalty': 'l2', 'C': 1.0, 'l1_ratio': 0.0, 'dual': False, 'tol': 1e-4, 'fit_intercept': True, 'intercept_scaling': 1.0, 'class_weight': None, 'random_state': None, 'solver': 'lbfgs', 'max_iter': 100, 'verbose': 0, 'warm_start': False, 'helpers': task.helpers()}))
+
+    # # model = Model(zip_file="C:/Users/prata/Files/Git/CausalBench/causalbench-asu/tests/model/decision_tree_classifier.zip")
+    # # out = model.execute(Bunch({'data': data_files.file1.copy(deep=True), 'criterion': 'gini', 'splitter': 'best', 'max_depth': None, 'min_samples_split': 2, 'min_samples_leaf': 1, 'min_weight_fraction_leaf': 0.0, 'max_features': None, 'random_state': None, 'max_leaf_nodes': None, 'min_impurity_decrease': 0.0, 'class_weight': None, 'ccp_alpha': 0.0, 'helpers': task.helpers()}))
 
     # model = Model(zip_file="C:/Users/prata/Files/Git/CausalBench/causalbench-asu/tests/model/linear_regression.zip")
     # out = model.execute(Bunch({'data': data_files.file1.copy(deep=True), 'fit_intercept': True, 'tol': 1e-6, 'positive': False, 'helpers': task.helpers()}))
     
-    # model = Model(zip_file="C:/Users/prata/Files/Git/CausalBench/causalbench-asu/tests/model/decision_tree_regressor.zip")
-    # out = model.execute(Bunch({'data': data_files.file1.copy(deep=True), 'criterion': 'squared_error', 'splitter': 'best', 'max_depth': None, 'min_samples_split': 2, 'min_samples_leaf': 1, 'min_weight_fraction_leaf': 0.0, 'max_features': None, 'random_state': None, 'max_leaf_nodes': None, 'min_impurity_decrease': 0.0, 'ccp_alpha': 0.0, 'helpers': task.helpers()}))
+    # # model = Model(zip_file="C:/Users/prata/Files/Git/CausalBench/causalbench-asu/tests/model/decision_tree_regressor.zip")
+    # # out = model.execute(Bunch({'data': data_files.file1.copy(deep=True), 'criterion': 'squared_error', 'splitter': 'best', 'max_depth': None, 'min_samples_split': 2, 'min_samples_leaf': 1, 'min_weight_fraction_leaf': 0.0, 'max_features': None, 'random_state': None, 'max_leaf_nodes': None, 'min_impurity_decrease': 0.0, 'ccp_alpha': 0.0, 'helpers': task.helpers()}))
 
-    # model = Model(zip_file="C:/Users/prata/Files/Git/CausalBench/causalbench-asu/tests/model/logistic_regression.zip")
-    # out = model.execute(Bunch({'data': data_files.file1.copy(deep=True), 'penalty': 'l2', 'C': 1.0, 'l1_ratio': 0.0, 'dual': False, 'tol': 1e-4, 'fit_intercept': True, 'intercept_scaling': 1.0, 'class_weight': None, 'random_state': None, 'solver': 'lbfgs', 'max_iter': 100, 'verbose': 0, 'warm_start': False, 'helpers': task.helpers()}))
+    # print()
+    # print()
+    # print()
+    # print('[Predicted]')
+    # print(out.output.prediction.data)
 
-    model = Model(zip_file="C:/Users/prata/Files/Git/CausalBench/causalbench-asu/tests/model/decision_tree_classifier.zip")
-    out = model.execute(Bunch({'data': data_files.file1.copy(deep=True), 'criterion': 'gini', 'splitter': 'best', 'max_depth': None, 'min_samples_split': 2, 'min_samples_leaf': 1, 'min_weight_fraction_leaf': 0.0, 'max_features': None, 'random_state': None, 'max_leaf_nodes': None, 'min_impurity_decrease': 0.0, 'class_weight': None, 'ccp_alpha': 0.0, 'helpers': task.helpers()}))
+    # print()
+    # print()
+    # print()
+    # print('[Ground Truth]')
+    # print(data_files.file1.copy(deep=True).data)
 
-    print()
-    print()
-    print()
-    print('[Predicted]')
-    print(out.output.prediction.data)
+    # # metric = Metric(zip_file="C:/Users/prata/Files/Git/CausalBench/causalbench-asu/tests/metric/accuracy_classification.zip")
+    # # out = metric.evaluate(Bunch({'ground_truth': data_files.file1.copy(deep=True), 'prediction': out.output.prediction, 'normalize': True, 'sample_weight': None, 'helpers': task.helpers()}))
 
-    print()
-    print()
-    print()
-    print('[Ground Truth]')
-    print(data_files.file1.copy(deep=True).data)
+    # # metric = Metric(zip_file="C:/Users/prata/Files/Git/CausalBench/causalbench-asu/tests/metric/precision_classification.zip")
+    # # out = metric.evaluate(Bunch({'ground_truth': data_files.file1.copy(deep=True), 'prediction': out.output.prediction, 'labels': None, 'pos_label': 1, 'average': 'macro', 'sample_weight': None, 'zero_division': 0, 'helpers': task.helpers()}))
+
+    # # metric = Metric(zip_file="C:/Users/prata/Files/Git/CausalBench/causalbench-asu/tests/metric/mae_regression.zip")
+    # # out = metric.evaluate(Bunch({'ground_truth': data_files.file1.copy(deep=True), 'prediction': out.output.prediction, 'sample_weight': None, 'multioutput': 'uniform_average', 'helpers': task.helpers()}))
+
+    # metric = Metric(zip_file="C:/Users/prata/Files/Git/CausalBench/causalbench-asu/tests/metric/r2score_regression.zip")
+    # out = metric.evaluate(Bunch({'ground_truth': data_files.file1.copy(deep=True), 'prediction': out.output.prediction, 'sample_weight': None, 'multioutput': 'uniform_average', 'helpers': task.helpers()}))
+
+    # print()
+    # print()
+    # print()
+    # print('[Score]')
+    # print(out.output.score)
+
+
+
+
+    # task1 = Task(zip_file="C:/Users/prata/Files/Git/CausalBench/causalbench-asu/tests/task/classification.zip", module_id=4, version=1)
     
-    # # model
-    # print('[MODEL]')
-    # data = data_files.file1.copy(deep=True)
+    # dataset1 = Dataset(zip_file="C:/Users/prata/Files/Git/CausalBench/causalbench-asu/tests/data/breast_cancer_wisconsin.zip")
+    # dataset2 = Dataset(zip_file="C:/Users/prata/Files/Git/CausalBench/causalbench-asu/tests/data/forest_covertype.zip")
 
-    # target = task.helpers().get_target(data).copy(deep=True)
-    # target[:] = 0
+    # model1 = Model(zip_file="C:/Users/prata/Files/Git/CausalBench/causalbench-asu/tests/model/logistic_regression.zip")
+    # model2 = Model(zip_file="C:/Users/prata/Files/Git/CausalBench/causalbench-asu/tests/model/decision_tree_classifier.zip")
 
-    # print(task.helpers().get_target(data))
+    # metric1 = Metric(zip_file="C:/Users/prata/Files/Git/CausalBench/causalbench-asu/tests/metric/accuracy_classification.zip")
+    # metric2 = Metric(zip_file="C:/Users/prata/Files/Git/CausalBench/causalbench-asu/tests/metric/precision_classification.zip")
 
-    # task.helpers().set_target(data, target)
-    # print(task.helpers().get_target(data))
+    # context1 = Context.create(
+    #     name='Classification 1',
+    #     description='Test classification context',
+    #     task=task1,
+    #     datasets=[(dataset1, {'data': 'file1', 'ground_truth': 'file1'}),
+    #               (dataset2, {'data': 'file1', 'ground_truth': 'file1'})],
+    #     models=[(model1, {}), (model2, {})],
+    #     metrics=[(metric1, {}), (metric2, {})]
+    # )
 
-    # print()
-    # print()
-    # print()
+    # run1 = context1.execute()
 
-    # print('[METRICS]')
-    # ground_truth = data_files.file1.copy(deep=True)
-    # predicted = data
+    # print(run1)
 
-    # target = task.helpers().get_target(ground_truth)
-    # print(target)
 
-    # target = task.helpers().get_target(predicted)
-    # print(target)
 
-    # print()
-
-    # features = task.helpers().get_features(ground_truth)
-    # print(features)
-
-    # features = task.helpers().get_features(predicted)
-    # print(features)
-
-    # task: Task = Task(module_id=1, version=1)
-    # # print(task.name)
-    # task.publish(public=True)
-
-    # dataset: Dataset = Dataset(zip_file="C:\\Users\\prata\\Files\\Git\\CausalBench\\causalbench-asu\\tests\\data\\abalone.zip")
-    # dataset.publish()
-
-    # dataset: Dataset = Dataset(module_id=1, version=1)
-    # # # print(dataset.name)
-    # dataset.publish(public=True)
-
-    # task: Task = Task(zip_file="C:\\Users\\prata\\Files\\Git\\CausalBench\\causalbench-asu\\tests\\task\\discovery.static.zip")
-    # task.publish()
-
-    # model: Model = Model(zip_file="C:\\Users\\prata\\Files\\Git\\CausalBench\\causalbench-asu\\tests\\model\\pc.zip")
-    # model.publish()
-
-    # model: Model = Model(module_id=1, version=1)
-    # # # print(model.name)
-    # model.publish(public=True)
-
-    # metric: Metric = Metric(zip_file="C:\\Users\\prata\\Files\\Git\\CausalBench\\causalbench-asu\\tests\\metric\\accuracy_static.zip")
-    # metric.publish()
-
-    # metric: Metric = Metric(module_id=1, version=1)
-    # # print(metric.name)
-    # metric.publish(public=True)
-
-    # context: Context = Context.create(
-    #     name='Context1',
-    #     description='Test static context',
-    #     task=task,
-    #     datasets=[(dataset, {'data': 'file1', 'ground_truth': 'file2'})],
-    #     models=[(model, {})],
-    #     metrics=[(metric, {})])
+    # task2 = Task(zip_file="C:/Users/prata/Files/Git/CausalBench/causalbench-asu/tests/task/regression.zip", module_id=5, version=1)
     
-    # context.publish()
-    
-    # # run: Run = context.execute()
-    # # print(run)
+    # dataset3 = Dataset(zip_file="C:/Users/prata/Files/Git/CausalBench/causalbench-asu/tests/data/california_housing.zip")
+    # dataset4 = Dataset(zip_file="C:/Users/prata/Files/Git/CausalBench/causalbench-asu/tests/data/diabetes.zip")
+
+    # model3 = Model(zip_file="C:/Users/prata/Files/Git/CausalBench/causalbench-asu/tests/model/linear_regression.zip")
+    # model4 = Model(zip_file="C:/Users/prata/Files/Git/CausalBench/causalbench-asu/tests/model/decision_tree_regressor.zip")
+
+    # metric3 = Metric(zip_file="C:/Users/prata/Files/Git/CausalBench/causalbench-asu/tests/metric/mae_regression.zip")
+    # metric4 = Metric(zip_file="C:/Users/prata/Files/Git/CausalBench/causalbench-asu/tests/metric/r2score_regression.zip")
+
+    # context = Context.create(
+    #     name='Regression 1',
+    #     description='Test classification context',
+    #     task=task2,
+    #     datasets=[(dataset3, {'data': 'file1', 'ground_truth': 'file1'}),
+    #               (dataset4, {'data': 'file1', 'ground_truth': 'file1'})],
+    #     models=[(model3, {}), (model4, {})],
+    #     metrics=[(metric3, {}), (metric4, {})]
+    # )
+
+    # run2 = context.execute()
+
+    # print(run2)
+
+
 
 
 if __name__ == '__main__':
